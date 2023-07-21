8436c4f4c14f4b52268acda672d0d2c295187abb
Florian Thauvin <72510511+Florian-Thauvin@users.noreply.github.com>
2023-07-21 16:15:04 +0200

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { TextView } from './widgets/input/text/TextView';
 import { toUpperCase } from './widgets/input/text/TextUtils';
+import { TableView } from './widgets/display/table/TableView';
 
 export function Demo() {
-    return <TextViewDemo />;
+    //return <TextViewDemo />;
+    return <TableViewDemo />;
 }
 
 export function TextViewDemo() {
@@ -16,7 +18,7 @@ export function TextViewDemo() {
                     isEnabled: false,
                     label: 'TextViewDisabled',
                     id: 'TextViewDisabled',
-                    setValue: (_value: string) => {},
+                    setValue: (_value: string) => {}
                 }}
             />
             <TextView
@@ -26,18 +28,18 @@ export function TextViewDemo() {
                     label: 'TextViewUpperCase',
                     id: 'TextViewUpperCase',
                     setValue: setFormatValue,
-                    value: formatValue,
+                    value: formatValue
                 }}
             />
             <TextView
                 {...{
                     isEnabled: true,
                     constraints: {
-                        isRequired: true,
+                        isRequired: true
                     },
                     label: 'TextViewRequired',
                     id: 'TextViewRequired',
-                    setValue: (_value: string) => {},
+                    setValue: (_value: string) => {}
                 }}
             />
             <TextView
@@ -45,11 +47,11 @@ export function TextViewDemo() {
                     isEnabled: true,
                     constraints: {
                         isRequired: false,
-                        maxSize: 5,
+                        maxSize: 5
                     },
                     label: 'TextViewMaxSize',
                     id: 'TextViewMaxSize',
-                    setValue: (_value: string) => {},
+                    setValue: (_value: string) => {}
                 }}
             />
             <TextView
@@ -57,13 +59,43 @@ export function TextViewDemo() {
                     isEnabled: true,
                     constraints: {
                         isRequired: false,
-                        regexp: /[A-Z]{5}/,
+                        regexp: /[A-Z]{5}/
                     },
                     label: 'TextViewRegexp',
                     id: 'TextViewRegexp',
-                    setValue: (_value: string) => {},
+                    setValue: (_value: string) => {}
                 }}
             />
         </>
+    );
+}
+
+interface IFakeData {
+    name: string;
+    secondname: string;
+}
+
+const data: IFakeData[] = [
+    { name: 'titi', secondname: 'a' },
+    { name: 'toto', secondname: 'a' },
+    { name: 'tata', secondname: 'a' },
+    { name: 'tata', secondname: 'b' }
+];
+
+export function TableViewDemo() {
+    return (
+        <TableView
+            {...{
+                id: '1',
+                data,
+                columns: [
+                    { attribute: 'name', display: (value: string) => <p>{value}</p>, index: 0, label: 'name' },
+                    { attribute: 'secondname', display: (value: string) => <p>{value}</p>, index: 0, label: 'secondname' }
+                ],
+                getIdentifier: (value: IFakeData) => {
+                    return value.name;
+                }
+            }}
+        />
     );
 }
